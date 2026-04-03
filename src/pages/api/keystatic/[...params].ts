5917cd94f483ef3727899dbe4ce12beb2abdb01a
kenandotfyi <kenan.akinci@icloud.com>
2026-04-03 15:26:45 +0200

    added env vars to handler to solve keystatic localhost redirect bug with github

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -3,5 +3,10 @@ import keystaticConfig from '../../../../keystatic.config'
 
 export const prerender = false
 
-const handler = makeHandler({ config: keystaticConfig })
+const handler = makeHandler({
+  config: keystaticConfig,
+  clientId: import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: import.meta.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: import.meta.env.KEYSTATIC_SECRET,
+})
 export const ALL = handler
